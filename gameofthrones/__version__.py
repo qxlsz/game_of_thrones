1d9be9237b29e9324f4c3067a25cd6b43e7696e7
Raj Josyula <rjosyula@tesla.com>
2019-04-22 20:32:30 -0400

    version with ascii game of thrones

---
+++ gameofthrones/__version__.py
@@ -0,0 +1,13 @@
+#  ___    __    __  __  ____    _____  ____    ____  _   _  ____  _____  _  _  ____  ___ 
+# / __)  /__\  (  \/  )( ___)  (  _  )( ___)  (_  _)( )_( )(  _ \(  _  )( \( )( ___)/ __)
+#( (_-. /(__)\  )    (  )__)    )(_)(  )__)     )(   ) _ (  )   / )(_)(  )  (  )__) \__ \
+# \___/(__)(__)(_/\/\_)(____)  (_____)(__)     (__) (_) (_)(_)\_)(_____)(_)\_)(____)(___/
+
+__title__ = 'gameofthrones'
+__description__ = 'Python library to retrieve game of thrones info'
+__url__ = 'www.rajasekharjosyula.com'
+__version__ = '0.0.1'
+__author__ = 'Rajasekhar Josyula'
+__author_email__ = 'rajasekhar.josyula@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 Rajasekhar Josyula'
